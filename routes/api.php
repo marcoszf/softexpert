87d0adfe790c53b8a8057584f8f4eab5c8d3c3f5
marcos colombelli basgal <marcos.colombelli@gmail.com>
2023-08-01 14:49:22 -0300

    Ajustar

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductTypeController;
+use App\Http\Controllers\Api\ProductTypeController;
 use App\Http\Controllers\ProductSaleController;
 
 /*
